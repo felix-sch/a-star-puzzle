5da71674951531ead96b8807b48567e90f56b72f
Felix <felixschaumann@aol.de>
2015-12-03 14:48:54 +0100

    Fixed algo

--- AStar.java
+++ AStar.java
@@ -1,12 +1,13 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.lang.Math;
+import java.io.IOException;
 
 class AStar {
   private static ArrayList<Puzzle> open = new ArrayList<Puzzle>();
   private static ArrayList<Puzzle> closed = new ArrayList<Puzzle>();
 
-  public static Puzzle calcShortestPath(Puzzle start, Puzzle goal) {
+  public static Puzzle calcShortestPath(Puzzle start, Puzzle goal) throws IOException {
 
     open.clear();
     closed.clear();
@@ -14,31 +15,69 @@ class AStar {
     open.add(start);
 
     while (open.size() != 0) {
+      System.out.println("===================================================");
+      System.out.format("Open list: %d elements\n", open.size());
 
       Puzzle current = open.get(0);
+      System.out.println(current);
 
       if (current.equals(goal)) {
+        System.out.println("Goal reached, returning!");
         return current;
       }
 
       open.remove(current);
       closed.add(current);
 
+      System.out.format("Open list: %d Closed list: %d\n",
+        open.size(), closed.size());
+
+      System.out.println("Looping over neighbors");
       for (Puzzle neighbor: getNeighbors(current)) {
 
-        if (closed.contains(neighbor))
+        if (closed.contains(neighbor)) {
+          System.out.println("Neighbor already in closed list, continuing");
           continue;
+        }
+
+        System.out.println(neighbor);
 
         neighbor.setHeuristic(calcHeuristic(neighbor));
+        System.out.println("Heuristic of neighbor: " + neighbor.getHeuristic());
 
         if (open.contains(neighbor)) {
-          open.set(open.indexOf(neighbor), neighbor);
+          //System.in.read();
+          System.out.println("Neighbor is in open list:");
+          Puzzle existing = open.get(open.indexOf(neighbor));
+          System.out.format("%d (existing) > %d (new) ?",
+            existing.getHeuristic() + existing.getSteps(),
+            neighbor.getHeuristic() + neighbor.getSteps());
+
+          if (existing.getHeuristic() + existing.getSteps()
+            > neighbor.getHeuristic() + neighbor.getSteps()) {
+            open.set(open.indexOf(neighbor), neighbor);
+            System.out.println("> Updated neighbor in open list");
+          } else {
+            System.out.println("> No action");
+          }
         } else {
           open.add(neighbor);
+          System.out.println("Neighbor is not in open list -> added");
         }
       }
 
+      System.out.println("Sorting open list");
+      for (Puzzle p: open) {
+        System.out.format("%d,", p.getSteps() + p.getHeuristic());
+      }
+      System.out.println();
+
       Collections.sort(open);
+
+      for (Puzzle p: open) {
+        System.out.format("%d,", p.getSteps() + p.getHeuristic());
+      }
+      System.out.println();
     }
 
     return null;
@@ -60,10 +99,6 @@ class AStar {
               puzzle.getNumbers().length + (puzzle.getNumbers().length - 1)) %
               puzzle.getNumbers().length};
 
-          System.out.format("Free field at (%d,%d) and %d should be at: (%d,%d)\n",
-            currentPosition[0], currentPosition[1],
-            puzzle.getLastNum(), desiredPosition[0], desiredPosition[1]);
-
           int smallerX = Math.min(currentPosition[0], Math.abs(puzzle.getNumbers().length-currentPosition[0]-1));
           int smallerY = Math.min(currentPosition[1], Math.abs(puzzle.getNumbers().length-currentPosition[1]-1));
           int shortestWallCurrent = Math.min(smallerX, smallerY);
@@ -72,12 +107,7 @@ class AStar {
           smallerY = Math.min(desiredPosition[1], Math.abs(puzzle.getNumbers().length-desiredPosition[1]-1));
           int shortestWallDesired = Math.min(smallerX, smallerY);
 
-
-          System.out.format("Move out %d and move in %d\n",
-            shortestWallCurrent, shortestWallDesired);
-
           heuristic += shortestWallCurrent + shortestWallDesired + 2;
-          System.out.println("Heuristic (current): " + heuristic);
           continue;
         }
 
@@ -86,10 +116,6 @@ class AStar {
           (currentNumber %
             puzzle.getNumbers().length + (puzzle.getNumbers().length - 1)) %
             puzzle.getNumbers().length};
-
-        System.out.format("Number: %d at (%d,%d) should be at: (%d,%d)\n",
-          currentNumber, currentPosition[0], currentPosition[1],
-          desiredPosition[0], desiredPosition[1]);
 
         int innerDistance = Math.abs(currentPosition[0] - desiredPosition[0]) +
           Math.abs(currentPosition[1] - desiredPosition[1]);
@@ -104,18 +130,15 @@ class AStar {
 
         int outerDistance = Math.min(shortestWallCurrent, shortestWallDesired) + 2;
         heuristic += Math.min(outerDistance, innerDistance);
-
-        System.out.format("Inner: %d Outer %d\n", innerDistance, outerDistance);
-
-        System.out.println("Heuristic (current): " + heuristic);
       }
     }
+
     return heuristic;
   }
 
   private static Puzzle[] getNeighbors(Puzzle current) {
     ArrayList<Puzzle> neighbors = new ArrayList<Puzzle>();
-    for (int i=0; i<5; i++) {
+    for (int i=0; i<current.getNumbers().length; i++) {
       neighbors.add(current.shiftRight(i));
       neighbors.add(current.shiftLeft(i));
       neighbors.add(current.shiftUp(i));
--- TestAStar.java
+++ TestAStar.java
@@ -1,25 +1,37 @@
+import java.io.IOException;
 
 public class TestAStar {
 
   public static void main(String[] args) {
 
     Puzzle start = new Puzzle(new int[][] {
+      {1,3,4},
+      {7,6,5},
+      {2,0,9}
+    }, 8);
+
+    Puzzle goal = new Puzzle(new int[][] {
       {1,2,3},
       {4,5,6},
       {7,8,9}
     }, 0);
 
-    Puzzle test = new Puzzle(new int[][] {
-      {2,3,4},
-      {1,5,6},
-      {7,8,0}
-    }, 9);
+    System.out.println(start);
+    Puzzle last = null;
+    try {
+      last = AStar.calcShortestPath(start, goal);
+    } catch(IOException e) { }
 
-    //System.out.println(start);
-    System.out.println(test);
+    if (last != null) {
+      System.out.println("Path:");
+      printPath(last);
+    }
+  }
 
-    System.out.println("Heuristic: " + AStar.calcHeuristic(test));
-
-    //AStar.calcShortestPath(start, start);
+  private static void printPath(Puzzle p) {
+    if (p.getParent() != null) {
+      printPath(p.getParent());
+    }
+    System.out.println(p);
   }
 }
